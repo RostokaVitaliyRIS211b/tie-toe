78de31a9cd93cd465554cb0bc1b0a0de2de71469
Виталий Ростока <rostoka.vit@yandex.ru>
2022-07-30 22:38:33 +0500

    op

--- tie-toe/Main.cs
+++ tie-toe/Main.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using tie_toe;
+using Lines;
 using g = SFML.Graphics;
 using a = SFML.Audio;
 using w = SFML.Window;
 using s = SFML.System;
+
 namespace Main
 {
     class Program
@@ -13,22 +16,56 @@ namespace Main
             int rank = 3, width_screen=600,height_screen=600;
             int side_of_cell = width_screen / rank;
             s.Vector2i mouse_pos = new s.Vector2i();
+            List<line> lines = new List<line>();
             g.RenderWindow MainWindow = new g.RenderWindow(new w.VideoMode((uint)width_screen, (uint)height_screen), "Tie-Toe");
             g.CircleShape circle = new g.CircleShape(30);
             circle.FillColor = g.Color.Blue;
             circle.Position = new s.Vector2f(1000, 700);
             MainWindow.KeyPressed += Window_KeyPressed;
+            MainWindow.MouseButtonPressed += MainWindow_MouseButtonPressed;
+            create_lines(width_screen,height_screen,out lines,side_of_cell);
             while (MainWindow.IsOpen)
             {
                 mouse_pos=w.Mouse.GetPosition(MainWindow);
                 MainWindow.Clear(g.Color.White);
                 MainWindow.DispatchEvents();
+                foreach (line line in lines)
+                    MainWindow.Draw(line);
                 MainWindow.Display();
                 //Console.WriteLine("GAY");
             }
             Console.ReadKey();
             return 0;
         }
+        private static void create_lines(int width_screen, int height_screen,out List<line> lines,int side_of_cell)
+        {
+            lines=new List<line>();
+            for(int i = 0; i <= width_screen; i+=side_of_cell)
+            {
+                line line1 = new line();
+                line1.point_one = new s.Vector2i(i, 0);
+                line1.point_two = new s.Vector2i(i, height_screen);
+                line1.color = new g.Color(0, 255, 255);
+                lines.Add(line1);
+            }
+            for (int i = 0; i <= height_screen; i += side_of_cell)
+            {
+                line line1 = new line();
+                line1.point_one = new s.Vector2i(0, i);
+                line1.point_two = new s.Vector2i(width_screen, i);
+                line1.color = new g.Color(0, 255, 255);
+                lines.Add(line1);
+            }
+        }
+        private static void MainWindow_MouseButtonPressed(object sender, w.MouseButtonEventArgs e)
+        { 
+           var window = (SFML.Window.Window)sender;
+           if (e.Button == w.Mouse.Button.Left)
+           {
+              
+           }
+        }
+
         private static void Window_KeyPressed(object sender, SFML.Window.KeyEventArgs e)
         {
             var window = (SFML.Window.Window)sender;
--- tie-toe/line.cs
+++ tie-toe/line.cs
@@ -1,22 +1,34 @@
 ﻿using g = SFML.Graphics;
 using w = SFML.Window;
 using s = SFML.System;
+using SFML.System;
+using SFML.Graphics;
+
 namespace Lines
 {
     internal class line:g.Drawable
     {
-        s.Vector2i point_one { get; set; }
-        s.Vector2i point_two { get; set; }
-        g.Color color { get; set; }
+        public s.Vector2i point_one { get; set; }
+        public s.Vector2i point_two { get; set; }
+        public g.Color color { get; set; }
+        public line()
+        {
+
+        }
+        public line(Vector2i point_one, Vector2i point_two, Color color)
+        {
+            this.point_one = point_one;
+            this.point_two = point_two;
+            this.color = color;
+        }
+
         public void Draw(g.RenderTarget target, g.RenderStates states)
         {
-            g.VertexArray lines=new g.VertexArray();
-            g.Vertex one = new g.Vertex(((s.Vector2f)point_one),color);
+            g.Vertex one = new g.Vertex(((s.Vector2f)point_one), color);
             g.Vertex two = new g.Vertex(((s.Vector2f)point_two), color);
-            lines.Append(one);
-            lines.Append(two);
+            g.Vertex[] lines1 = {one,two}; 
             states = g.RenderStates.Default;
-            target.Draw(lines,states);
+            target.Draw(lines1,g.PrimitiveType.Lines);
         }
 
     }
